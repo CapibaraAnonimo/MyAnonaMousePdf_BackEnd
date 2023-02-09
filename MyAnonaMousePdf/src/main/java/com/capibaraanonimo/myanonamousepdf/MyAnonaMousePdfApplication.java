59ce345c4d71ccb8410405aaf8754df5323305d0
CapibaraNonimo <adrianarnaizcano@gmail.com>
2023-02-09 08:29:18 +0100

    Creado el proyecto de spring para la api de MyAnonaMousePdf

---
+++ MyAnonaMousePdf/src/main/java/com/capibaraanonimo/myanonamousepdf/MyAnonaMousePdfApplication.java
@@ -0,0 +1,13 @@
+package com.capibaraanonimo.myanonamousepdf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyAnonaMousePdfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyAnonaMousePdfApplication.class, args);
+	}
+
+}
---
+++ MyAnonaMousePdf/src/test/java/com/capibaraanonimo/myanonamousepdf/MyAnonaMousePdfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capibaraanonimo.myanonamousepdf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyAnonaMousePdfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
