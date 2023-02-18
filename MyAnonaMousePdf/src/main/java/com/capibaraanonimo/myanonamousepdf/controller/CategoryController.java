5fc1c00bfbc5fb954ecbb4ec01f68eea76df74ab
CapibaraAnonimo <adrianarnaizcano@gmail.com>
2023-02-18 14:01:57 +0100

    Añadida la búsqueda de categoría por id y añadidas todas las categorías al import.sql

--- MyAnonaMousePdf/src/main/java/com/capibaraanonimo/myanonamousepdf/controller/CategoryController.java
+++ MyAnonaMousePdf/src/main/java/com/capibaraanonimo/myanonamousepdf/controller/CategoryController.java
@@ -4,10 +4,12 @@ import com.capibaraanonimo.myanonamousepdf.dto.CategoryResponse;
 import com.capibaraanonimo.myanonamousepdf.service.CategoryService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.UUID;
 
 @RestController()
 @RequestMapping("/category")
@@ -18,5 +20,10 @@ public class CategoryController {
     @GetMapping()
     public List<CategoryResponse> getAll() {
         return categoryService.findAllWithBooks();
+    }
+
+    @GetMapping("/{id}") //TODO comprobar que va con postgresql
+    public CategoryResponse getById(@PathVariable String id) {
+        return CategoryResponse.of(categoryService.findById(UUID.fromString(id)));
     }
 }
